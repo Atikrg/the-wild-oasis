c96e223a816c478f78e4d76ba88fdcc89cd69b21
Atikrg <email>
2024-07-01 00:14:31 +0530

    438. Optimizing Image with Next.js

--- app/_components/logo.js
+++ app/_components/logo.js
@@ -1,17 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
-
+import logo from "@/public/logo.png"
 function Logo() {
     return (
         <>
-            <Link
-                href="/"
-                className="flex items-center gap-5 z-10"
-            >
-                <img
+            <Link href="/" className="flex items-center gap-5 z-10">
+            {/*     <Image
                     src="/logo.png"
                     alt="The Wild Oasis Logo"
                     width={60}
+                    height={60}
+                /> */}
+                <Image
+                    src={logo}
+                    alt="The Wild Oasis Logo"
+                    width={60}
+                    quality={100}
                     height={60}
                 />
             </Link>

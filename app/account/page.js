dff1442c2b2d3bde4647f4ae6900fde9196a019a
Atikrg <email>
2024-07-01 01:54:29 +0530

    441. Adding nested routes and pages

--- app/account/page.js
+++ app/account/page.js
@@ -1,3 +1,9 @@
-export default function Page(){
-    return <h1>This is a Account Page</h1>
+export default function Page() {
+    return (
+        <>
+            <h2 className="font-semibold text-2xl text-orange-400 mb-7">
+                Welcome, Atik Salim Rangnekar
+            </h2>
+        </>
+    );
 }

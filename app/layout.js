ac1da745007ac200cc06f764d5f3e42e3f2e2426
Atikrg <email>
2024-07-01 00:37:27 +0530

    439. Building the Home Pag

--- app/layout.js
+++ app/layout.js
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body
-                className={`
-            bg-black text-gray-100 min-h-screen flex flex-col
+                className={` antialiased
+            bg-black text-gray-100 min-h-screen flex flex-col relative
             `}
             >
                 <Header />
--- app/page.js
+++ app/page.js
@@ -1,11 +1,35 @@
 import Image from "next/image";
 import Link from "next/link";
 import Navigation from "./_components/navigation";
+import bg from "@/public/bg.png";
 export default function Home() {
-  return (
-    <>
-    <h1>Welcome to wild oasis!</h1>
-    <Link href="/cabins">Explore Luxury cabins</Link>
-    </>
-  );
+    return (
+        <>
+            <main className="mt-24">
+                <Image
+                    src={bg}
+                    fill
+                    className="object-cover object-top"
+                    placeholder="blur"
+                    quality={80}
+                    alt="Mountains and forests with two cabins"
+                />
+
+                <div className="relative z-10 text-center">
+                    <h1 className="text-8xl text-primary-50 mb-10 tracking-tight font-normal">
+                        Welcome to paradise.
+                    </h1>
+
+                    <Link
+                        href="/cabins"
+                        className="bg-orange-300 px-8 py-6 text-black text-lg font-semibold hover:bg-orange-600 transition-all"
+                    >
+                        Explore Luxury cabins
+                    </Link>
+                </div>
+            </main>
+            <h1>Welcome to wild oasis!</h1>
+            <Link href="/cabins">Explore Luxury cabins</Link>
+        </>
+    );
 }
